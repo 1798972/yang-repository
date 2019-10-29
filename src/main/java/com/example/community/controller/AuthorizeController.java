404df3677d8421588da32d8cf222b91b539f4d55
1798972 <1798972@qq.com>
2019-10-29 22:51:42 +0800

    yangyang1111111

---
+++ src/main/java/com/example/community/controller/AuthorizeController.java
@@ -0,0 +1,44 @@
+package com.example.community.controller;
+
+import com.example.community.dto.AccessTokenDTO;
+import com.example.community.dto.GitHubUser;
+import com.example.community.provider.GitHubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    //类GitHubProvider需要先添加@Component注解
+    @Autowired
+    private GitHubProvider gitHubProvider;
+
+    //application.properties文件中的属性值进行注入
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state){
+
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+
+        String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);   //得到用户的token
+        GitHubUser user = gitHubProvider.getUser(accessToken);
+        System.out.println(user.getName()+"---"+user.getBio());
+        return "index";
+    }
+}
---
+++ src/main/java/com/example/community/provider/GitHubProvider.java
@@ -0,0 +1,51 @@
+package com.example.community.provider;
+
+import com.alibaba.fastjson.JSON;
+import com.example.community.dto.AccessTokenDTO;
+import com.example.community.dto.GitHubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+
+@Component
+public class GitHubProvider {  //得到用户token
+    public String getAccessToken(AccessTokenDTO accessTokenDTO){
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+        //RequestBody对象转换成json对象
+            RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
+            Request request = new Request.Builder()
+                    .url("https://github.com/login/oauth/access_token")
+                    .post(body)
+                    .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            System.out.println(string);
+            String token =  string.split("&")[0].split("=")[1];
+            System.out.println("解析出token:"+token);
+            return token;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public GitHubUser getUser(String accessToken){  //得到对应的github用户
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token="+accessToken)  //这个网址不要写错了
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            System.out.println(string);
+            //string json格式解析成gitHubUser对象
+            GitHubUser gitHubUser = JSON.parseObject(string,GitHubUser.class);
+            return gitHubUser;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
