81c28aedacb187ed0ea232dab14c45a76bbe68c6
1798972 <1798972@qq.com>
2020-02-26 14:15:42 +0800

    腾讯云对象存储

--- src/main/java/com/example/community/controller/FileController.java
+++ src/main/java/com/example/community/controller/FileController.java
@@ -1,6 +1,9 @@
 package com.example.community.controller;
 
 import com.example.community.dto.UploadDTO;
+import com.example.community.exception.CustomizeErrorCode;
+import com.example.community.exception.CustomizeException;
+import com.example.community.provider.FileProvider;
 import com.example.community.provider.QCloudProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
+
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
@@ -21,26 +25,31 @@ import java.io.IOException;
 public class FileController {
     @Autowired
     private QCloudProvider qCloudProvider;
+
     @ResponseBody
     @PostMapping("/file/upload")
-    public UploadDTO uploadFile(HttpServletRequest request){
+    public UploadDTO uploadFile(HttpServletRequest request) throws IOException {
         MultipartHttpServletRequest multipartHttpServletRequest = (MultipartHttpServletRequest) request;
         //MultipartFile 这个类一般是用来接受前台传过来的文件
+        //在spring上传文件中，一般都使用了MultipartFile来接收
         MultipartFile file = multipartHttpServletRequest.getFile("editormd-image-file");
 
         //进行文件的上传操作
         try {
-            qCloudProvider.upload(file.getInputStream() != null ? file.getInputStream()  : null,file.getName());
-        } catch (IOException e) {
-            e.printStackTrace();
+            //转换成File
+            File targetFile = FileProvider.multipartFileToFile(file);
+            String url = qCloudProvider.upload(targetFile != null ? targetFile : null, file.getOriginalFilename());
+            FileProvider.delteTempFile(targetFile);
+
+            //返回成功的json
+            UploadDTO uploadDTO = new UploadDTO();
+            uploadDTO.setSuccess(1);
+            uploadDTO.setMessage("上传成功！");
+            uploadDTO.setUrl(url);
+
+            return uploadDTO;
+        } catch (Exception e) {
+            throw new CustomizeException(CustomizeErrorCode.UPLOAD_ERROR);
         }
-
-        //返回成功的json
-        UploadDTO uploadDTO = new UploadDTO();
-        uploadDTO.setSuccess(1);
-        uploadDTO.setMessage("成功！");
-        uploadDTO.setUrl("/img/vxImage.png");
-
-        return uploadDTO;
     }
 }
--- src/main/java/com/example/community/provider/QCloudProvider.java
+++ src/main/java/com/example/community/provider/QCloudProvider.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.InputStream;
+import java.net.URL;
+import java.util.Date;
 import java.util.UUID;
 
 
@@ -27,38 +29,50 @@ public class QCloudProvider {
     @Value("${qcloud.secretkey}")
     private String secretkey;
 
-    //桶的名字
-    String bucketName = "yang-cdisk-1258124344";
     /*// 指定要上传到 COS 上对象键
     static String key = "mydemo.jpg";*/
 
     // 简单文件上传, 最大支持 5 GB, 适用于小文件上传, 建议 20M以下的文件使用该接口
     //file里面填写本地图片的位置 我这里是相对项目的位置，在项目下有src/test/demo.jpg这张图片
-    public String upload(InputStream inputStream, String fileName) {
+    public String upload(File file, String fileName) {
         // 1 初始化用户身份信息
         COSCredentials cred = new BasicCOSCredentials(secretid, secretkey);
         // 2 设置bucket的区域
-        ClientConfig clientConfig = new ClientConfig(new Region("ap-chengdu"));
+        ClientConfig clientConfig = new ClientConfig(new Region("ap-guangzhou"));
         // 3 生成cos客户端
         COSClient cosClient = new COSClient(cred, clientConfig);
+        //桶的名字
+        String bucketName = "yang-cdisk-1258124344";
 
-        //InputStream处理文件
-        File localFile = new File("");
         //名字的处理
-        String targetName ;
-        String[] path = fileName.split("//.");
+        String targetName;
+        String[] path = fileName.split("\\.");  //点的转义 \\.
         if (path.length > 1) {
+            //targetName   a77aebed-77c9-41e5-8f1b-07286cbc8fbc.png
             targetName = UUID.randomUUID().toString() + "." + path[path.length - 1];
-        }else {
+        } else {
             return null;
         }
         //动态生成对象键名
         String key = targetName;
-        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key, localFile);
+        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key, file);
         PutObjectResult putObjectResult = cosClient.putObject(putObjectRequest);
-        String etag = putObjectResult.getETag();  // 获取文件的 etag
-        System.out.println("信息----------"+etag);
-        return etag;
+        //etag    fdd68975c693dcb355cd6c4dfee6f3ab
+//        String etag = putObjectResult.getETag();  // 获取文件的 etag
+//        return etag;
+
+        //关闭后返回文件链接  要开启公有读的属性
+        cosClient.shutdown();
+        Date expiration = new Date(new Date().getTime() + 24 * 60 * 60 * 365 *10 );
+        URL oldurl = cosClient.generatePresignedUrl(bucketName, key, expiration);
+        // 获得链接后解析字符串并且返回
+        // 先将ur
+        String url = oldurl.toString();
+        // http://jobpic-1258185724.cos.ap-guangzhou.myqcloud.com/image/T.jpg?sign
+        // 直接查找到第一个？的位置
+        url = url.substring(0, url.indexOf("?"));
+        // 开始解析字符串
+        return url;
     }
 
     // 设置要下载到的本地路径
